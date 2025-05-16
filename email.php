009852a5a7f616c8f8a04ffde4395361be4fd2ec
Naresh Narnapati <naresh.narnapati@reymould.com>
2025-05-16 17:18:38 +0530

    navigation ,icons and map are updated

--- email.php
+++ email.php
@@ -1,14 +1,12 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
+  
     $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
+
 
     $to = "naresh.narnapati@reymould.com";
     $subject = "Email enquiry from Hygenixseeds.com website";
-    $body = "Name: $firstName $lastName\nEmail: $email\nPhone: $phone\nMessage:\n$message";
+    $body = "Email: $email";
 
     $emailSent = mail($to, $subject, $body);
 
